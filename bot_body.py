067df175883f29074fed8cc8abc9ac509bbd9fe3
Danil-114195722 <dudkovdy@gmail.com>
2023-02-05 11:36:23 +0000

    add printing date in bot_state

--- bot_body.py
+++ bot_body.py
@@ -33,7 +33,7 @@ async def help_command(message: types.Message):
 
 @disp.message_handler()
 async def main_command(message: types.Message):
-    word = message.text.lower()
+    word = message.text.lower().strip()
 
     try:
         if re.match(r'[а-я]', word):
@@ -74,10 +74,11 @@ if __name__ == '__main__':
     with open('./bot_state', 'w') as bot_state:
         now_time = localtime()
 
-        bot_state.write('Start in')
+        bot_state.write('Start in\n')
         # для локалки
         # bot_state.write(f' {now_time.tm_hour}.{now_time.tm_min}.{now_time.tm_sec}\n')
         # для сервака
-        bot_state.write(f' {now_time.tm_hour + 3}.{now_time.tm_min}.{now_time.tm_sec}\n')
+        bot_state.write(f'Time: {now_time.tm_hour + 3}.{now_time.tm_min}.{now_time.tm_sec}\n')
+	bot_state.write(f'Date: {now_time.tm_mday}.{now_time.tm_mon}.{now_time.tm_year}\n')
 
     executor.start_polling(disp)
